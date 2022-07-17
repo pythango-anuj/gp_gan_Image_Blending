538b742505b664ede7c4f8fb85cb760a484f50ad
pythango-anuj <2018034@iiitdmj.ac.in>
2022-07-17 23:18:12 +0000

    Training model

--- create_image_set.py
+++ create_image_set.py
@@ -11,11 +11,11 @@ parser.add_argument('--input', help='Path to input image.', default='')
 args = parser.parse_args()
 src_dir= '/content/GP-GAN/DataBase/DentraTrainingData/data/GT/*.jpeg'
 images = glob.glob(src_dir)
-nf=71
+nf=76
 j=0
 for image in images:
     j+=1
-    if j<=71:
+    if j<=76:
       continue
     args.input = image
     image = cv.imread(cv.samples.findFile(args.input))
